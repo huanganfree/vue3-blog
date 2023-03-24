e35d9efc4591ae12d78c731b8433cb85ac141931
huanganfree <huangan1206@163.com>
2023-03-24 17:16:46 +0800

    添加404页面

---
+++ src/components/MainPage.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <router-view />
+    <TabBar />  
+  </div>
+</template>
+<script setup>
+import TabBar from './TabBar.vue'
+
+</script>
---
+++ src/views/errorPage/error404.vue
@@ -0,0 +1,28 @@
+<template>
+  <van-empty
+    image="error"
+  >
+    <template #description>
+      <div class="description">
+        404
+      </div>
+    </template>
+    <van-button
+      round
+      type="primary"
+      class="bottom-button"
+      @click="$router.push('/login')"
+    >
+      返回登录
+    </van-button>
+  </van-empty>
+</template>
+<style lang="scss" scoped>
+.bottom-button {
+  width: 160px;
+  height: 40px;
+}
+.description{
+  font-size:23px;
+}
+</style>
