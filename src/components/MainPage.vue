d82bcfc456aca8641586f2aba06739c32d0ec22f
huanganfree <huangan1206@163.com>
2023-03-29 18:34:17 +0800

    增加导航栏组件

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,10 +1,28 @@
 <template>
   <div>
     <router-view />
-    <TabBar />  
+    <!-- 如果整个MainPage组件卸载掉，下面的过渡不触发 -->
+    <Transition>
+      <TabBar v-if="['/main/home', '/main/me', '/main/photo'].includes(route.path)" /> 
+    </Transition> 
   </div>
 </template>
-<script setup>
-import TabBar from './TabBar.vue'
+<style lang="scss" scoped>
+.v-enter-active,
+.v-leave-active {
+  transition: all 0.4s;
+}
 
+.v-enter-from,
+.v-leave-to {
+  transform: translateY(100px);
+}
+</style>
+
+<script setup>
+// eslint-disable-next-line no-unused-vars
+import TabBar from './TabBar.vue'
+import { useRoute } from "vue-router";
+// eslint-disable-next-line no-unused-vars
+const route = useRoute()
 </script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <van-nav-bar
+    :title="title"
+    left-arrow
+    fixed
+    @click-left="clickLeftFn"
+  />
+</template>
+
+<script>
+import { useRouter } from "vue-router";
+export default {
+  name: "Nav",
+  props:{
+    title:{
+      type: String,
+      default: '标题'
+    },
+    onClickLeft: {
+      type: [Function, undefined],
+      default: undefined
+    }
+  },
+  setup(props) {
+    const router = useRouter()
+    const clickLeftFn = props.onClickLeft || (() => router.go(-1))
+    return {
+      clickLeftFn
+    }
+  }
+};
+</script>
