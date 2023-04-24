c28af5c103a4c85ea079fdc570520bd09ec468d1
huanganfree <huangan1206@163.com>
2023-04-24 13:15:25 +0800

    更改配置文件

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/my-project/blog/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/my-project/blog/' : '/',
+  pages: {
+    index: {
+      // page 的入口
+      entry: 'src/main.js',
+      // 模板来源
+      template: 'public/index.html',
+      // 在 dist/index.html 的输出
+      filename: 'index.html',
+      title: 'vue3-express'
+    }
+  }
 }
