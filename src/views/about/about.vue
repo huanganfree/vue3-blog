4fc2426166acd5a5b30ced0f665b6aa8f05440e1
huanganfree <huangan1206@163.com>
2025-06-11 22:28:16 +0800

    feat: 完成数据懒加载功能

--- src/views/about/about.vue
+++ src/views/about/about.vue
@@ -4,44 +4,42 @@
       title="关于我们"
       :on-click-left="() => $router.push('/main/me')"
     />
-    <van-pull-refresh
-      v-model="isLoading"
-      @refresh="onRefresh"
-    >
-      <div class="article-wrapper">
-        <div v-if="listData.map(item => item.article).join('').length">
-          <div
+    <div class="article-wrapper">
+      <div v-if="listData.length">
+        <van-list
+          v-model:loading="loading"
+          :finished="finished"
+          finished-text="没有更多了"
+          :immediate-check="true"
+          :offset="10"
+          @load="initData"
+        >
+          <van-cell
             v-for="item in (listData)"
             :key="item.id"
             class="article"
-          >
-            {{ item.article }}
-          </div>
-        </div>
-        <van-empty
-          v-else
-          image="error"
-          description="暂无数据"
-        />
+            :title="item.article"
+          />
+        </van-list>
       </div>
-      <template #success>
-        <van-icon
-          name="checked"
-          color="#4fc08d"
-        />
-        <span style="margin-left: 10px;">刷新成功</span>
-      </template>
-    </van-pull-refresh>
+      <van-empty
+        v-else
+        image="error"
+        description="暂无数据"
+      />
+    </div>
   </div>
 </template>
 <style lang="scss" scoped>
-.about{
+.about {
   padding: 46px 15px 0 15px;
 }
-.article-wrapper{
+
+.article-wrapper {
   height: calc(100vh - 47px);
 }
-.article{
+
+.article {
   font-size: 14px;
   line-height: 23px;
   text-indent: 2em;
@@ -51,34 +49,48 @@
 import NavBar from '@/components/NavBar.vue'
 import { requestAbout } from '@/api/about.js'
 export default {
-  components:{
+  components: {
     NavBar
   },
   data() {
     return {
       listData: [],
-      isLoading: false
+      loading: false,
+      finished: true,
+      pageSize: 10,
+      pageNum: 1
     };
   },
   mounted() {
     this.initData()
   },
-  methods:{
-    initData(){
-      this.isLoading = true
-      requestAbout()
+  methods: {
+    initData() {
+      this.loading = true
+      requestAbout({
+        pageSize: this.pageSize,
+        pageNum: this.pageNum
+      })
         .then(res => {
-          this.isLoading = false
           const { code, message, data } = res
-          if(code == 200){
-            this.listData = data || []
+          if (code == 200) {
+            this.loading = false
+            const { records, total } = data
+            this.listData = this.listData.concat(records || [])
+            if(total == this.listData.length) {
+              this.finished = true
+            } else {
+              this.pageNum += 1
+              this.finished = false
+            }
           } else {
             this.listData = []
+            this.pageNum = 1
             message && this.$notify(message)
           }
         })
     },
-    onRefresh(){
+    onRefresh() {
       this.initData()
     }
   }
